971308f2516e220c6be649b189910b4760e77052
Leks389 <147524543+Leks389@users.noreply.github.com>
2023-10-24 16:52:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const buttonSetColor = document.getElementById("button-set-color");
 
+function setStyleColor() {
+square.style.backgroundColor = "blue";
+}
+function changeColor() {
+button.classList.toggle("square-blue");
+}
+
+buttonSetColor.addEventListener("click", changeColor);
